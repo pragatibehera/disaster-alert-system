dba3d5ded7b6d5b6460ee947716a07a3eaa827f3
mrinal-mann <mrinal.mann24@gmail.com>
2025-04-16 13:45:45 +0530

    hydration fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
